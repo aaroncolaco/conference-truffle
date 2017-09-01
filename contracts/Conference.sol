5c95ab5be009885c360f9811debb02aca2bdd3c1
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-09-01 18:18:59 +0530

    Add Conference contract and Migration

---
+++ contracts/Conference.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.11;
+
+contract Conference {
+  address public organizer;
+  mapping (address => uint) public registrantsPaid;
+  uint public numRegistrants;
+  uint public quota;
+
+  event Deposit(address _from, uint _amount);
+  event Refund(address _to, uint _amount);
+
+  modifier onlyOrganizer() {
+    require(msg.sender == organizer);
+    _;
+  }
+
+  function Conference() {
+    organizer = msg.sender;
+    quota = 500;
+    numRegistrants = 0;
+  }
+
+  function buyTicket() public payable returns (bool) {
+    if (numRegistrants >= quota) {return false;}
+
+    registrantsPaid[msg.sender] = msg.value;
+    numRegistrants++;
+    Deposit(msg.sender, msg.value);
+    return true;
+  }
+
+  function changeQuota(uint newquota) public onlyOrganizer {
+    quota = newquota;
+  }
+
+  function refundTicket(address recipient, uint amount) public onlyOrganizer {
+    if (registrantsPaid[recipient] == amount) {
+      address myAddress = this;
+      if (myAddress.balance >= amount) {
+        recipient.transfer(amount);
+        registrantsPaid[recipient] = 0;
+        numRegistrants--;
+        Refund(recipient, amount);
+      }
+    }
+  }
+
+  function destroy() { // so funds not locked in contract forever
+    if (msg.sender == organizer) {
+      selfdestruct(organizer); // send funds to organizer
+    }
+  }
+}
