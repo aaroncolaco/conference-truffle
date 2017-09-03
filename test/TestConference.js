e45573e9066c9bba6a74f8bb64614e920832d723
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-09-03 11:52:05 +0530

    Add test cases for refund; Refactor code

--- test/TestConference.js
+++ test/TestConference.js
@@ -2,7 +2,7 @@
 
 const conferenceContract = artifacts.require("Conference");
 
-contract('Conference', function (accounts) {
+contract('Conference', (accounts) => {
 
   const account_zero = accounts[0];
   const account_one = accounts[1];
@@ -40,42 +40,42 @@ contract('Conference', function (accounts) {
   });
 
   describe('check ticket purchase', () => {
-    it("should return `true` on ticket purchase", function () {
+    it("should return `true` on ticket purchase", () => {
       return conferenceContract.deployed()
-        .then(function (instance) {
+        .then(instance => {
           return instance.buyTicket.call({ value: 20, from: account_one });  // using `call()` so that we can get return value. Otherwise returns Tx object
         })
-        .then(function (returnValue) {
+        .then((returnValue) => {
           assert.equal(returnValue, true, 'did not return `true` on ticket purchase');
         });
     });
 
-    it("should emit `Deposit` event on ticket purchase", function () {
+    it("should emit `Deposit` event on ticket purchase", () => {
       return conferenceContract.deployed()
-        .then(function (instance) {
+        .then(instance => {
           return instance.buyTicket({ value: 20, from: account_one });
         })
-        .then(function (transaction) {
+        .then((transaction) => {
           assert.equal(transaction.logs[0].event, 'Deposit', 'did not emit a `Deposit` event');
         });
     });
 
-    it("should record amount paid for ticket purchased", function () {
+    it("should record amount paid for ticket purchased", () => {
       return conferenceContract.deployed()
-        .then(function (instance) {
+        .then(instance => {
           return instance.registrantsPaid(account_one);
         })
-        .then(function (amountPaid) {
+        .then(amountPaid => {
           assert.equal(amountPaid, 20, 'did not record ticket purchase amount correctly');
         });
     });
 
-    it("should increment `numRegistrants`", function () {
+    it("should increment `numRegistrants`", () => {
       return conferenceContract.deployed()
-        .then(function (instance) {
+        .then(instance => {
           return instance.numRegistrants.call();
         })
-        .then(function (numRegistrants) {
+        .then((numRegistrants) => {
           assert.equal(numRegistrants, 1, 'did not increment `numRegistrants` after purchase');
         });
     });
@@ -83,27 +83,80 @@ contract('Conference', function (accounts) {
 
   describe('check quota update', () => {
     it("owner can update quota", () => {
+      let contractInstance = null;
       return conferenceContract.deployed()
         .then(instance => {
-          return instance.changeQuota(650)
-            .then(transaction => {
-              return instance.quota.call();
-            })
+          contractInstance = instance;
+          return instance.changeQuota(650);
         })
+        .then(transaction => contractInstance.quota.call())
         .then(quota => {
           assert.equal(quota, 650, 'did not update quota');
         });
     });
     it("others cannot update quota", () => {
+      let contractInstance = null;
       return conferenceContract.deployed()
         .then(instance => {
-          return instance.changeQuota(700, { from: account_one })
-            .then(transaction => {
-              return instance.quota.call();
-            })
+          contractInstance = instance;
+          return instance.changeQuota(700, { from: account_one });
         })
+        .then(transaction => contractInstance.quota.call())
         .then(quota => {
           assert.equal(quota, 650, 'did not update quota');
+        });
+    });
+  });
+
+  describe('check refund', () => {
+    it("others cannot refund partial amount", () => {
+      let contractInstance = null;
+      return conferenceContract.deployed()
+        .then(instance => {
+          contractInstance = instance;
+          return instance.refundTicket(account_one, 10, { from: account_one });
+        })
+        .then(transaction => contractInstance.registrantsPaid(account_one))
+        .then(amount => {
+          assert.equal(amount, 20, 'should not have refunded amount');
+        });
+    });
+    it("others cannot refund full amount", () => {
+      let contractInstance = null;
+      return conferenceContract.deployed()
+        .then(instance => {
+          contractInstance = instance;
+          return instance.refundTicket(account_one, 20, { from: account_one });
+        })
+        .then(transaction => contractInstance.registrantsPaid(account_one))
+        .then(amount => {
+          assert.equal(amount, 20, 'should not have refunded amount');
+        });
+    });
+
+    it("organizer cannot refund partial amount", () => {
+      let contractInstance = null;
+      return conferenceContract.deployed()
+        .then(instance => {
+          contractInstance = instance;
+          return instance.refundTicket(account_one, 10);
+        })
+        .then(transaction => contractInstance.registrantsPaid(account_one))
+        .then(amountPaid => {
+          assert.equal(amountPaid, 20, 'should not have refunded partial amount');
+        });
+    });
+
+    it("organizer can refund full amount", () => {
+      let contractInstance = null;
+      return conferenceContract.deployed()
+        .then(instance => {
+          contractInstance = instance;
+          return instance.refundTicket(account_one, 20);
+        })
+        .then(transaction => contractInstance.registrantsPaid(account_one))
+        .then(amountPaid => {
+          assert.equal(amountPaid, 0, 'should have refunded full amount');
         });
     });
   });
